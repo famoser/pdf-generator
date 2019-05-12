34b5a6afaa91cb9aa985e96a97acd662ef43d251
Florian Moser <products@famoser.ch>
2019-05-12 19:57:36 +0200

    restructure

---
+++ src/Font/Frontend/Table/FontDirectory.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the famoser/pdf-generator project.
+ *
+ * (c) Florian Moser <git@famoser.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PdfGenerator\Font\IR\Table;
+
+/**
+ * the font directly is at the start of the TTF file and defines where which tables are located.
+ *
+ * @see https://developer.apple.com/fonts/TrueType-Reference-Manual/RM06/Chap6.html
+ * @see https://docs.microsoft.com/en-us/typography/opentype/spec/otff
+ */
+class FontDirectory
+{
+    private $offsetTable
+}
--- src/Font/Frontend/Table/OffsetTable.php
+++ src/Font/Frontend/Table/OffsetTable.php
@@ -12,11 +12,12 @@
 namespace PdfGenerator\Font\IR\Table;
 
 /**
- * the font directly is at the start of the TTF file and defines where which tables are located.
+ * the offset table (also called sfnt) defines how many tables the font consists of.
  *
  * @see https://developer.apple.com/fonts/TrueType-Reference-Manual/RM06/Chap6.html
  * @see https://docs.microsoft.com/en-us/typography/opentype/spec/otff
  */
-class FontDirectory
+class OffsetTable
 {
+    private
 }
