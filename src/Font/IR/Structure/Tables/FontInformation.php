00668a0f5b00c07635608b715c5ecf76b8fd2522
Florian Moser <products@famoser.ch>
2019-06-30 10:58:09 +0200

    add properties

---
+++ src/Font/IR/Structure/Tables/FontInformation.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the famoser/pdf-generator project.
+ *
+ * (c) Florian Moser <git@famoser.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PdfGenerator\Font\IR\Structure\Tables;
+
+class FontInformation
+{
+    private $language;
+
+    private $copyrightNotice;
+
+    private $family;
+
+    private $subfamily;
+
+    private $identifier;
+
+    private $fullName;
+
+    private $version;
+
+    private $postscriptName;
+
+    private $trademarkNotice;
+
+    private $manufacturer;
+
+    private $designer;
+
+    private $description;
+
+    private $urlVendor;
+
+    private $urlDesigner;
+
+    private $licenseDescription;
+    private $licenseUrl;
+    private $typographicFamily;
+    private $typographicSubfamily;
+    private $compatibleFull;
+    private $sampleText;
+    private $postscriptCIDName;
+    private $wwsFamilyName;
+    private $wwsSubfamilyName;
+    private $lightBackgroundPalette;
+    private $darkBackgroundPalette;
+    private $variationsPostScriptNamePrefix;
+}
