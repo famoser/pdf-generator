2e1fec5a17f6373e0a0ac4457b8d26f8ca3f9288
Florian Moser <git@famoser.ch>
2023-06-09 16:05:35 +0200

    feat: Add rector

---
+++ rector.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Set\ValueObject\LevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__.'/document-generator',
+        __DIR__.'/resources',
+        __DIR__.'/scripts',
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ]);
+
+    // define sets of rules
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_82
+    ]);
+};
