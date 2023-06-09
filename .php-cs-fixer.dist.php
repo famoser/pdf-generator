06d6a69d8a7415545ba81e6956c4be63bb969945
Florian Moser <git@famoser.ch>
2023-06-09 13:31:53 +0200

    chore: Reformat

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -12,13 +12,12 @@ COMMENT;
 $finder = PhpCsFixer\Finder::create()
     ->in('document-generator')
     ->in('src')
-    ->in('tests')
-;
+    ->in('tests');
 
 $config = new PhpCsFixer\Config();
 return $config->setRules([
-        '@Symfony' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'header_comment' => ['header' => $fileHeaderComment, 'separate' => 'both'],
-    ])
+    '@Symfony' => true,
+    'array_syntax' => ['syntax' => 'short'],
+    'header_comment' => ['header' => $fileHeaderComment, 'separate' => 'both'],
+])
     ->setFinder($finder);
