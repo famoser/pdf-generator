5b783719dffae4c51dc94076d1772f657c15f4a5
Florian Moser <git@famoser.ch>
2023-06-18 12:46:57 +0200

    feat: Implement measurement for blocks and flows

--- src/Frontend/LayoutEngine/Measure/Measurement.php
+++ src/Frontend/LayoutEngine/Measure/Measurement.php
@@ -14,27 +14,27 @@ namespace PdfGenerator\Frontend\LayoutEngine\Measure;
 readonly class Measurement
 {
     /**
-     * @param float|null $weight    set by flowing content (e.g. text)
-     *                              intuitively represents the approximate area covered by the element
-     *                              typically include the minimal area measured, multiplied by a factor depending on how elastic the content flows
-     * @param float|null $minWidth  set by fixed-size content or if parent forces a height
-     * @param float|null $minHeight set by fixed-size content or if the parent forces a width
+     * @param float $weight    set by flowing content (e.g. text)
+     *                         intuitively represents the approximate area covered by the element
+     *                         typically include the minimal area measured, multiplied by a factor depending on how elastic the content flows
+     * @param float $minWidth  the min width required to place content
+     * @param float $minHeight the min height required to place content
      */
-    public function __construct(private ?float $weight, private ?float $minWidth, private ?float $minHeight)
+    public function __construct(private float $weight, private float $minWidth, private float $minHeight)
     {
     }
 
-    public function getWeight(): ?float
+    public function getWeight(): float
     {
         return $this->weight;
     }
 
-    public function getMinWidth(): ?float
+    public function getMinWidth(): float
     {
         return $this->minWidth;
     }
 
-    public function getMinHeight(): ?float
+    public function getMinHeight(): float
     {
         return $this->minHeight;
     }
