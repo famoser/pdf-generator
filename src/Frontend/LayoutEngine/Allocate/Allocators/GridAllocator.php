3e7aa24dc758de300f7518dcc8fb68221da4c8d2
Florian Moser <git@famoser.ch>
2023-12-30 15:49:11 +0100

    feat: Add auto columns

--- src/Frontend/LayoutEngine/Allocate/Allocators/GridAllocator.php
+++ src/Frontend/LayoutEngine/Allocate/Allocators/GridAllocator.php
@@ -15,6 +15,7 @@ use PdfGenerator\Frontend\Layout\Grid;
 use PdfGenerator\Frontend\Layout\Style\ColumnSize;
 use PdfGenerator\Frontend\LayoutEngine\Allocate\BlockAllocation;
 use PdfGenerator\Frontend\LayoutEngine\Allocate\BlockAllocationVisitor;
+use PdfGenerator\Frontend\LayoutEngine\Measure\BlockMeasurementVisitor;
 
 class GridAllocator
 {
@@ -27,29 +28,17 @@ class GridAllocator
      */
     public function allocate(Grid $grid, array &$overflowRows = [], float &$usedWidth = 0, float &$usedHeight = 0): array
     {
-        if (0 === count($grid->getRows())) {
+        if (0 === \count($grid->getRows())) {
             return [];
         }
 
         $columnSizes = $this->getColumnSizes($grid);
-        $distributeWidth = $this->getDistributeWidth($columnSizes, $grid);
 
-        /** @var BlockAllocation[][] $blockAllocationsPerColumn */
-        $blockAllocationsPerColumn = [];
         $widths = [];
-        foreach ($columnSizes as $columnIndex => $columnSize) {
-            if (ColumnSize::MINIMAL !== $columnSize) {
-                continue;
-            }
-
-            $usedColumnWidth = 0;
-            $blockAllocationsPerColumn[$columnIndex] = $this->allocateMinColumn($grid, $columnIndex, $distributeWidth, $usedColumnWidth);
-            $widths[$columnIndex] = $usedColumnWidth;
-            $distributeWidth -= $usedColumnWidth;
-        }
+        $blockAllocationsPerColumn = $this->allocatedBlocksPerColumn($grid, $columnSizes, $widths);
 
         $heights = [];
-        foreach ($blockAllocationsPerColumn as $column => $blockAllocations) {
+        foreach ($blockAllocationsPerColumn as $blockAllocations) {
             foreach ($blockAllocations as $row => $blockAllocation) {
                 $heights[$row] = isset($heights[$row]) ? max($heights[$row], $blockAllocation->getHeight()) : $blockAllocation->getHeight();
             }
@@ -60,48 +49,138 @@ class GridAllocator
         $overflowRows = $grid->getRows();
         $usedHeight = 0;
         foreach ($heights as $row => $height) {
-            $progressMade = count($allocatedBlocks) > 0;
+            $progressMade = \count($allocatedBlocks) > 0;
             $overflow = $usedHeight + $height > $this->height;
             if ($overflow && $progressMade) {
-                $usedHeight = $grid->getPerpendicularGap();
                 break;
             }
 
             array_shift($overflowRows);
 
             $currentWidth = 0;
-            foreach ($blockAllocationsPerColumn as $column => $blockAllocations) {
-                if (isset($blockAllocations[$row])) {
-                    $blockAllocation = BlockAllocation::shift($blockAllocations[$row], $currentWidth, $usedHeight);
+            foreach (array_keys($columnSizes) as $columnIndex) {
+                if (isset($blockAllocationsPerColumn[$columnIndex][$row])) {
+                    $blockAllocation = BlockAllocation::shift($blockAllocationsPerColumn[$columnIndex][$row], $currentWidth, $usedHeight);
 
                     $allocatedBlocks[] = $blockAllocation;
                 }
 
-                $currentWidth += $widths[$column] + $grid->getGap();
+                $currentWidth += $widths[$columnIndex] + $grid->getGap();
             }
 
             $usedWidth = max($usedWidth, $currentWidth - $grid->getGap());
             $usedHeight += $height + $grid->getPerpendicularGap();
         }
 
+        if ($usedHeight > 0) {
+            $usedHeight -= $grid->getPerpendicularGap();
+        }
+
         return $allocatedBlocks;
+    }
+
+    /**
+     * @param (ColumnSize|string|numeric)[] $columnSizes
+     *
+     * @return BlockAllocation[][]
+     */
+    private function allocatedBlocksPerColumn(Grid $grid, array $columnSizes, array &$widths): array
+    {
+        $numberOfGaps = \count($columnSizes) - 1;
+        $availableWidth = $this->width - $grid->getGap() * $numberOfGaps;
+
+        // allocate fixed size or minimal size columns
+        $blockAllocationsPerColumn = array_fill(0, \count($columnSizes), []);
+        $widths = array_fill(0, \count($columnSizes), 0);
+        $toBeMeasuredColumns = [];
+        foreach ($columnSizes as $columnIndex => $columnSize) {
+            if (ColumnSize::MINIMAL !== $columnSize && !\is_numeric($columnSize)) {
+                $toBeMeasuredColumns[] = $columnIndex;
+                continue;
+            }
+
+            $usedColumnWidth = 0;
+            $blockAllocationsPerColumn[$columnIndex] = $this->allocateColumn($grid, $columnIndex, $availableWidth, $usedColumnWidth);
+
+            $columnWidth = ColumnSize::MINIMAL === $columnSize ? $usedColumnWidth : $columnSize;
+            $widths[$columnIndex] = $columnWidth;
+            $availableWidth -= $columnWidth;
+        }
+
+        // measure auto and unit columns
+        $expectedMaxWeight = ($this->width - $availableWidth) * $this->height;
+        $totalWeight = 0;
+        /** @var float[] $blockMeasurementsPerColumn */
+        $weightPerColumn = array_fill(0, \count($columnSizes), 0);
+        $measurer = new BlockMeasurementVisitor();
+        foreach ($grid->getRows() as $row) {
+            foreach ($toBeMeasuredColumns as $toBeMeasuredColumn) {
+                if (!$row->tryGet($toBeMeasuredColumn)) {
+                    continue;
+                }
+
+                $measurement = $row->tryGet($toBeMeasuredColumn)->accept($measurer);
+                $weightPerColumn[$toBeMeasuredColumn] += $measurement->getWeight();
+                $totalWeight += $measurement->getWeight();
+            }
+
+            if ($totalWeight > $expectedMaxWeight) {
+                break;
+            }
+        }
+
+        // distribute remaining weight to auto and unit columns
+        /** @var float[] $unitsPerColumn */
+        $unitsPerColumn = [];
+        $totalUnits = 0;
+        $totalUnitsColumnSize = 0;
+        foreach ($toBeMeasuredColumns as $columnIndex) {
+            $optimalColumnWidth = $totalWeight ? $weightPerColumn[$columnIndex] / $totalWeight * $availableWidth : 0;
+            $columnSize = $columnSizes[$columnIndex];
+            if (ColumnSize::AUTO === $columnSize) {
+                $widths[$columnIndex] = $optimalColumnWidth;
+                $usedWidth = 0;
+                $blockAllocationsPerColumn[$columnIndex] = $this->allocateColumn($grid, $columnIndex, $optimalColumnWidth, $usedWidth);
+            } elseif (str_ends_with($columnSize, ColumnSize::UNIT->value)) {
+                $units = floatval($columnSize);
+                $unitsPerColumn[$columnIndex] = $units;
+                $totalUnits += $units;
+                $totalUnitsColumnSize += $optimalColumnWidth;
+            } else {
+                $error = 'Unknown column size: '.$columnSize.'.';
+                $explanation = 'Must be either a number, a ColumnSize, or a unit (number*, e.g. 2*).';
+                assert(false, $error.' '.$explanation);
+            }
+        }
+
+        // allocate unit columns
+        if ($totalUnits > 0) {
+            $columnSizePerUnit = $totalUnitsColumnSize / $totalUnits;
+            foreach ($unitsPerColumn as $columnIndex => $units) {
+                $width = $columnSizePerUnit * $units;
+                $widths[$columnIndex] = $width;
+                $blockAllocationsPerColumn[$columnIndex] = $this->allocateColumn($grid, $columnIndex, $width, $usedWidth);
+            }
+        }
+
+        return $blockAllocationsPerColumn;
     }
 
     /**
      * @return BlockAllocation[][]
      */
-    private function allocateMinColumn(Grid $grid, int $columnIndex, float $availableWidth, float &$usedWidth): array
+    private function allocateColumn(Grid $grid, int $columnIndex, float $availableWidth, float &$usedWidth): array
     {
         $blockAllocations = [];
         $availableHeight = $this->height;
         foreach ($grid->getRows() as $rowIndex => $row) {
-            if (!array_key_exists($columnIndex, $row->getColumns())) {
+            if (!$row->tryGet($columnIndex)) {
                 continue;
             }
 
             $blockAllocator = new BlockAllocationVisitor($availableWidth, $availableHeight);
             /** @var BlockAllocation $blockAllocation */
-            $blockAllocation = $row->getColumns()[$columnIndex]->accept($blockAllocator);
+            $blockAllocation = $row->tryGet($columnIndex)->accept($blockAllocator);
 
             // abort if not enough space, but progress made
             $progressMade = $rowIndex > 0;
@@ -134,21 +213,5 @@ class GridAllocator
         }
 
         return $columnSizes;
-    }
-
-    /**
-     * @param (ColumnSize|float)[] $columnSizes
-     */
-    public function getDistributeWidth(array $columnSizes, Grid $grid): float
-    {
-        $numberOfGaps = count($columnSizes) - 1;
-        $distributeWidth = $this->width - $grid->getGap() * $numberOfGaps;
-        foreach ($columnSizes as $columnSize) {
-            if (is_numeric($columnSize)) {
-                $distributeWidth -= $columnSize;
-            }
-        }
-
-        return $distributeWidth;
     }
 }
