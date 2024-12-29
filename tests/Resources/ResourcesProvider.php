10f1be566f045fca2332ce234ea7443f2a739ab2
Florian Moser <git@famoser.ch>
2024-12-29 10:46:42 +0100

    maint: Increase static analysis level

--- tests/Resources/ResourcesProvider.php
+++ tests/Resources/ResourcesProvider.php
@@ -15,11 +15,13 @@ class ResourcesProvider
 {
     public static function getImage1Path(): string
     {
+        /** @phpstan-ignore-next-line  */
         return realpath(__DIR__.\DIRECTORY_SEPARATOR.'images'.\DIRECTORY_SEPARATOR.'image1.jpg');
     }
 
     public static function getFontOpenSansPath(): string
     {
+        /** @phpstan-ignore-next-line  */
         return realpath(__DIR__.\DIRECTORY_SEPARATOR.'fonts'.\DIRECTORY_SEPARATOR.'OpenSans.ttf');
     }
 }
