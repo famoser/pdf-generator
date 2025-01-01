0f6caa00657916149a5e6c59354778fea1f22eac
Florian Moser <git@famoser.ch>
2025-01-01 20:25:24 +0100

    test: Test content

--- tests/Integration/Frontend/ContentTest.php
+++ tests/Integration/Frontend/ContentTest.php
@@ -16,24 +16,27 @@ use Famoser\PdfGenerator\Frontend\Content\ImagePlacement;
 use Famoser\PdfGenerator\Frontend\Content\Rectangle;
 use Famoser\PdfGenerator\Frontend\Content\Style\DrawingStyle;
 use Famoser\PdfGenerator\Frontend\Content\Style\TextStyle;
-use Famoser\PdfGenerator\Frontend\Content\TextBlock;
 use Famoser\PdfGenerator\Frontend\Layout\AbstractElement;
 use Famoser\PdfGenerator\Frontend\Layout\Block;
 use Famoser\PdfGenerator\Frontend\Layout\ContentBlock;
 use Famoser\PdfGenerator\Frontend\Layout\Style\ElementStyle;
 use Famoser\PdfGenerator\Frontend\Layout\Text;
-use Famoser\PdfGenerator\Frontend\LinearDocument;
+use Famoser\PdfGenerator\Frontend\Document;
 use Famoser\PdfGenerator\Frontend\Resource\Font;
 use Famoser\PdfGenerator\Frontend\Resource\Image;
 use Famoser\PdfGenerator\IR\Document\Content\Common\Color;
+use Famoser\PdfGenerator\Tests\Integration\Frontend\TestUtils\Render;
 use Famoser\PdfGenerator\Tests\Resources\ResourcesProvider;
+use PHPUnit\Framework\TestCase;
 
-class ContentTestCase extends LinearDocumentTestCase
+class ContentTest extends TestCase
 {
+    use Render;
+
     public function testPrintRectangle(): void
     {
         // arrange
-        $document = new LinearDocument();
+        $document = new Document();
 
         // act
         $rectangleStyle = new DrawingStyle(lineColor: new Color(0, 255, 255), fillColor: new Color(0, 255, 0));
@@ -44,14 +47,14 @@ class ContentTestCase extends LinearDocumentTestCase
 
         // assert
         $result = $this->render($document);
-        $this->assertStringContainsString('1 0 0 1 22 235 cm 1 w 0 0 1 RG 1 0 0 rg 0 0 20 40 re b', $result);
-        $this->assertStringContainsString('1 0 0 1 5 5 cm 0 1 1 RG 0 1 0 rg 0 0 10 30 re b', $result);
+        $this->assertStringContainsString('1 0 0 1 22 225 cm 1 w 0 0 1 RG 1 0 0 rg 0 0 30 50 re b', $result);
+        $this->assertStringContainsString('1 0 -0 1 5 5 cm 0 1 1 RG 0 1 0 rg 0 0 20 40 re b', $result);
     }
 
     public function testPrintImagePlacement(): void
     {
         // arrange
-        $document = new LinearDocument();
+        $document = new Document();
 
         // act
         $image = Image::createFromFile(ResourcesProvider::getImage1Path());
@@ -62,62 +65,49 @@ class ContentTestCase extends LinearDocumentTestCase
 
         // assert
         $result = $this->render($document);
-        $this->assertStringContainsString('20 0 0 30 5 5 cm /I Do', $result);
+        $this->assertStringContainsString('30 0 -0 40 5 5 cm /I Do', $result);
     }
 
     public function testPrintTextAlign(): void
     {
         // arrange
-        $document = new LinearDocument();
-        $dummyText = 'PDF ist ein Textformat, strukturiert ähnlich wie XML, einfach etwas weniger Struktur. Hier kommt noch mehr Text, um das right align wirklich gut auszuprobieren.  Hier           kommt noch mehr Text, um das right align wirklich gut auszuprobieren. ';
+        $document = new Document();
+        $dummyText = 'This PDF has justified text, which means each line has exactly the same width. This result is reached by balancing the space in between the words; making it longer if else the last word on the line would not be fully to the right of the block of text.';
 
         // act
         $font = Font::createFromDefault();
-        $normalText = new TextStyle($font, 5, 1.2, new Color(0, 0, 0));
-
-        $text = new Text(alignment: Text\Alignment::ALIGNMENT_LEFT);
-        $text->add($normalText, $dummyText);
-        $document->add($this->createHighlightedBlock($text));
-
-        $text = new Text(alignment: Text\Alignment::ALIGNMENT_CENTER);
-        $text->add($normalText, $dummyText);
-        $document->add($this->createHighlightedBlock($text));
-
-        $text = new Text(alignment: Text\Alignment::ALIGNMENT_RIGHT);
-        $text->add($normalText, $dummyText);
-        $document->add($this->createHighlightedBlock($text));
+        $normalText = new TextStyle($font, new Color(0, 0, 0));
 
         $text = new Text(alignment: Text\Alignment::ALIGNMENT_JUSTIFIED);
-        $text->add($normalText, $dummyText.$dummyText.$dummyText);
+        $text->addSpan($dummyText, $normalText);
         $document->add($this->createHighlightedBlock($text));
 
         // assert
         $result = $this->render($document);
-        $this->assertStringContainsString('1 0 0 1 22 260.68 cm 1 w 0 0 1 RG 1 0 0 rg 0 0 122.704 14.32 re b', $result);
-        $this->assertStringContainsString('1 0 0 1 5 7.133 cm BT 0 0 0 rg /F 3 Tf 4.32 TL (PDF ist ein Textformat', $result);
+        $this->assertStringContainsString('1 0 0 1 22 203.88 cm 2 w 0 0 1 RG 1 0 0 rg 0 0 166 71.12 re b', $result);
+        $this->assertStringContainsString('Tw 11.52 TL (This PDF has justified text, which means each)Tj', $result);
     }
 
     public function testPrintPhrases(): void
     {
         // arrange
-        $document = new LinearDocument();
+        $document = new Document();
 
         // act
         $font = Font::createFromDefault();
-        $normalText = new TextStyle($font, 3, 2, new Color(0, 0, 0));
-        $bigText = new TextStyle($font, 20, 1, new Color(0, 0, 0));
-        $text = new Text();
-        $text->add($normalText, 'PDF ist ein Textformat, strukturiert ähnlich wie XML, einfach etwas weniger Struktur. ');
-        $text->add($bigText, 'Am besten einmal ein kleines PDF im ');
-        $text->add($normalText, 'Texteditor öffnen und durchschauen. Zum Beispiel vom Kontoauszug, diese PDFs haben oft etwas weniger komischer binary Anteil wie dies z.B. Tex generierte Dokumente haben.');
+        $style = new TextStyle($font);
+        $text = new Text(alignment: Text\Alignment::ALIGNMENT_JUSTIFIED);
+        $text->addSpan('PDF ist ein Textformat, strukturiert ähnlich wie XML, einfach etwas weniger Struktur. ', $style, 3);
+        $text->addSpan('Am besten einmal ein kleines PDF im ', $style, 10);
+        $text->addSpan('Texteditor öffnen und durchschauen. Zum Beispiel vom Kontoauszug, diese PDFs haben oft etwas weniger komischer binary Anteil wie dies z.B. Tex generierte Dokumente haben.', $style, 3);
 
         $contentBlock = $this->createHighlightedBlock($text);
         $document->add($contentBlock);
 
         // assert
         $result = $this->render($document);
-        $this->assertStringContainsString('1 0 0 1 22 178.6 cm', $result);
-        $this->assertStringContainsString('1 -0 0 1 5 89.213 cm BT', $result);
+        $this->assertStringContainsString('BT 1 0 0 1 1 31.15 cm 0 0 0 rg /F 3 Tf 0.228 Tw 14.4 TL (PDF ist ein Textformat, strukturiert', $result);
+        $this->assertStringContainsString('/F 10 Tf 0.76 Tw (Am besten)Tj 3.087 0 (einmal ein kleines PDF', $result);
     }
 
 
@@ -135,12 +125,12 @@ class ContentTestCase extends LinearDocumentTestCase
 
     private function createHighlightedBlock(AbstractElement $content): Block
     {
-        $highlightBlockStyle = new ElementStyle(1.0, new Color(0, 0, 255), new Color(255, 0, 0));
+        $highlightBlockStyle = new ElementStyle(2.0, new Color(0, 0, 255), new Color(255, 0, 0));
 
         $contentBlock = new Block($content);
         $contentBlock->setStyle($highlightBlockStyle);
         $contentBlock->setMargin([7, 7, 7, 7]);
-        $contentBlock->setPadding([0, 0, 0, 0]);
+        $contentBlock->setPadding([1, 1, 1, 1]);
 
         return $contentBlock;
     }
