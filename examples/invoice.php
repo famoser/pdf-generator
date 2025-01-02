584963bdfa3b248f010470e9aea85c1e574f4e16
Florian Moser <git@famoser.ch>
2025-01-02 11:10:13 +0100

    feat: Work on examples

--- examples/invoice.php
+++ examples/invoice.php
@@ -11,7 +11,6 @@
 
 include '../vendor/autoload.php';
 
-use Famoser\PdfGenerator\Frontend\Content\Rectangle;
 use Famoser\PdfGenerator\Frontend\Content\Style\DrawingStyle;
 use Famoser\PdfGenerator\Frontend\Content\Style\TextStyle;
 use Famoser\PdfGenerator\Frontend\Document;
@@ -21,13 +20,14 @@ use Famoser\PdfGenerator\Frontend\Layout\Style\ColumnSize;
 use Famoser\PdfGenerator\Frontend\Layout\Style\FlowDirection;
 use Famoser\PdfGenerator\Frontend\Layout\Table;
 use Famoser\PdfGenerator\Frontend\Layout\Text;
-use Famoser\PdfGenerator\Frontend\Resource\Meta;
 use Famoser\PdfGenerator\Frontend\Resource\Font;
+use Famoser\PdfGenerator\Frontend\Resource\Meta;
 use Famoser\PdfGenerator\IR\Document\Content\Common\Color;
 
 $margin = 15;
 $meta = Meta::basic('en', 'Invoice UZH-ZI-5', ['Florian Moser']);
 $document = new Document([210, 297], $margin, $meta);
+$flow = new Flow(FlowDirection::COLUMN);
 
 $normalFont = Font::createFromDefault();
 $normalText = new TextStyle($normalFont);
@@ -35,25 +35,32 @@ $secondaryText = new TextStyle($normalFont, Color::createFromHex('#6c757d'));
 $boldFont = Font::createFromDefault(Font\FontFamily::Helvetica, Font\FontWeight::Bold);
 $boldText = new TextStyle($boldFont);
 
-$sectionMargin = [10, 0, 0, 0];
+$textFontSize = 4.5;
+$headerFontSize = $textFontSize * 1.6;
+$spacer = $headerFontSize;
 
 // source address
-$text = new Text();
-$text->addSpan("famoser GmbH\n", $boldText);
-$text->addSpan("c/o Florian Moser\nMoosburgstrasse 25\n8307 Effretikon\nSchweiz", $normalText);
-$allocation = $document->allocate($text);
-$document->createPrinter()
-    ->position(210-30-$allocation->getWidth()) // shift right-most
-    ->print($allocation->getContent()[0]);
+$text = new Text(alignment: Text\Alignment::ALIGNMENT_RIGHT);
+$text->addSpan("famoser GmbH\n", $boldText, $textFontSize);
+$text->addSpan("c/o Florian Moser\nOchsengasse 66\n4123 Allschwil\nSchweiz", $normalText, $textFontSize);
+$flow->add($text);
 
 // target address
-$printer = $document->createPrinter(60);
-$printer->printText("Universität Zürich\nZentrale Informatik\nStampfenbachstrasse 73\n8006 Zürich\nSchweiz", $normalText);
+$text = new Text();
+$text->addSpan("Universität Zürich\nZentrale Informatik\nStampfenbachstrasse 73\n8006 Zürich\nSchweiz", $normalText, $textFontSize);
+$text->setMargin([0, $spacer*2, 0, 0]);
+$flow->add($text);
 
-// header
-$printer = $printer->position(top: 40);
-$printer->printText('Invoice UZH-ZI-5', $boldText);
-$printer = $printer->position(top: 8);
+// title
+$text = new Text(Text\Structure::Title);
+$text->addSpan('Invoice UZH-ZI-5', $boldText, $headerFontSize);
+$text->setMargin([0, $headerFontSize, 0, 0]);
+$flow->add($text);
+
+$document->add($flow);
+
+// print overview
+$printer = $document->createPrinter();
 $printer->printRectangle(180, 0.1, new DrawingStyle(0.1));
 
 $columns = [
@@ -68,7 +75,8 @@ foreach ($columns as $column) {
     $columnPrinter = $columnPrinter->position(45);
 }
 
-$printer = $printer->position(top: 15);
+$document->shiftPosition(15);
+$printer = $document->createPrinter();
 $printer->printRectangle(180, 0.1, new DrawingStyle(0.1));
 
 // description
@@ -81,12 +89,12 @@ $thankYou = new Text(Text\Structure::Paragraph);
 $thankYou->addSpan("To whom it may concern\n\nI would like to thank you for the order and the trust you have placed in me. The tasks according to the offer of 01.01.2023 were implemented on time and accepted by the client on 30.06.2023. Only the work actually incurred will be invoiced.", $normalText);
 $flow->add($thankYou);
 
-$document->setPosition(130);
+$flow->setMargin([0, $spacer, 0, 0]);
 $document->add($flow);
 
 // cost positions
 $table = new Table([ColumnSize::MINIMAL, ColumnSize::AUTO, ColumnSize::MINIMAL, ColumnSize::MINIMAL, ColumnSize::MINIMAL]);
-$table->setMargin([0, 3, 0, 5]);
+$table->setMargin([0, $spacer, 0, $spacer]);
 $textByRow = [
     [$boldText, 'Pos.', 'Description', 'Quantity', 'Unit price', 'Price'],
     [$normalText, '1', 'Concept work: Requirements engineering, UX concept, PoC', '12 h', '160.00', '1\'920.00'],
@@ -95,18 +103,20 @@ $textByRow = [
     [$secondaryText, '', 'plus VAT 14%', '', '', '1\'030.40'],
     [$boldText, '', 'Total', '', '', '8\'390.40'],
 ];
+$verticalPadding = $spacer / 20;
 foreach ($textByRow as $rowIndex => $rowDefinition) {
     $row = new Row();
     $textStyle = array_shift($rowDefinition);
     foreach ($rowDefinition as $index => $text) {
         $regards = new Text();
         $regards->addSpan($text, $textStyle);
-        $regards->setPadding([1, 0.3, 1, 0.3]);
+        $startPadding = $index > 0 ? $spacer / 2 : 0;
+        $regards->setPadding([$startPadding, $verticalPadding, 0, $verticalPadding]);
 
         $row->set($index, $regards);
     }
 
-    if ($rowIndex === 0) {
+    if (0 === $rowIndex) {
         $table->addHead($row);
     } else {
         $table->addBody($row);
