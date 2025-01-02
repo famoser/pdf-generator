7e9a16a94da1e664da9303c3521f38c11dcd682c
Florian Moser <git@famoser.ch>
2025-01-02 09:57:47 +0100

    fix: Fix flow test

--- tests/Integration/Frontend/FlowTest.php
+++ tests/Integration/Frontend/FlowTest.php
@@ -19,9 +19,12 @@ use Famoser\PdfGenerator\Frontend\Document;
 use Famoser\PdfGenerator\Frontend\Layout\Block;
 use Famoser\PdfGenerator\Frontend\Layout\ContentBlock;
 use Famoser\PdfGenerator\Frontend\Layout\Flow;
+use Famoser\PdfGenerator\Frontend\Layout\Style\ElementStyle;
 use Famoser\PdfGenerator\Frontend\Layout\Style\FlowDirection;
 use Famoser\PdfGenerator\Frontend\Layout\Text;
+use Famoser\PdfGenerator\Frontend\Layout\Text\Alignment;
 use Famoser\PdfGenerator\Frontend\Resource\Font;
+use Famoser\PdfGenerator\IR\Document\Content\Common\Color;
 use Famoser\PdfGenerator\Tests\Integration\Frontend\TestUtils\Render;
 use PHPUnit\Framework\TestCase;
 
@@ -56,8 +59,6 @@ class FlowTest extends TestCase
 
     public function testPrintSimpleFlowText(): void
     {
-        // TODO: why spacing wrong after "vom" (note too much space)
-
         // arrange
         $document = new Document();
 
@@ -65,7 +66,7 @@ class FlowTest extends TestCase
         $normalText = new TextStyle($font);
 
         // act
-        $paragraph = new Text(alignment: Text\Alignment::ALIGNMENT_JUSTIFIED);
+        $paragraph = new Text(alignment: Alignment::ALIGNMENT_JUSTIFIED);
         $paragraph->addSpan('PDF ist ein Textformat, strukturiert ähnlich wie XML, einfach etwas weniger Struktur. ', $normalText);
         $paragraph->addSpan('Am besten einmal ein kleines PDF im Texteditor öffnen und durchschauen. Zum Beispiel vom ', $normalText);
         $paragraph->addSpan('Kontoauszug', $normalText);
@@ -80,9 +81,8 @@ class FlowTest extends TestCase
 
         // assert
         $result = $this->render($document);
-        $this->assertStringContainsString('1 0 0 1 0 -60.96 cm BT', $result);
-        $this->assertStringContainsString('(Jeder dieser)Tj (Inhalte', $result);
-        $this->assertStringContainsString('1 0 0 1 0 -60.96 cm BT (Eine', $result);
+        $this->assertStringContainsString('0.533 Tw 5.76 TL (PDF ist e', $result);
+        $this->assertStringContainsString('en. Zum Beispiel vom)"', $result);
     }
 
     public function testPrintFlowText(): void
@@ -90,7 +90,7 @@ class FlowTest extends TestCase
         // TODO: why do columns stop on 2nd page?
 
         // arrange
-        $document = new Document();
+        $document = new Document(margin: 5);
 
         $font = Font::createFromDefault();
         $normalText = new TextStyle($font);
@@ -98,35 +98,39 @@ class FlowTest extends TestCase
         // act
         /** @var TextBlock[] $paragraphs */
         $paragraphs = [];
-        $paragraph = new Text();
+        $paragraph = new Text(alignment: Alignment::ALIGNMENT_JUSTIFIED);
         $paragraph->addSpan('PDF ist ein Textformat, strukturiert ähnlich wie XML, einfach etwas weniger Struktur. ', $normalText);
         $paragraph->addSpan('Am besten einmal ein kleines PDF im Texteditor öffnen und durchschauen. Zum Beispiel vom ', $normalText);
         $paragraph->addSpan('Kontoauszug', $normalText);
         $paragraph->addSpan(', diese PDFs haben oft etwas weniger komischer binary Anteil wie dies z.B. Tex generierte Dokumente haben.', $normalText);
         $paragraphs[] = $paragraph;
 
-        $paragraph = new Text();
+        $paragraph = new Text(alignment: Alignment::ALIGNMENT_RIGHT);
         $paragraph->addSpan('Es würde mich nicht erstaunen, wenn das meiste über das Format von solchen simplen PDFs selber zusammengereimt werden kann: Abgesehen von den Auswüchsen wie Formulare oder Schriftarten ist es nämlich ganz schön simpel gehalten. ', $normalText);
         $paragraph->addSpan('Der Parser muss eigentlich nur Dictionaries (key-value Datenstruktur) und Streams (binary blobs) verstehen. ', $normalText);
         $paragraph->addSpan('Das ist praktisch: Die meisten PDFs Dateien sind streng genommen fehlerhaft generiert, und in dem die Parsers nur diese beiden Objekte unterscheiden müssen, können trotzdem die allermeisten PDFs angezeigt werden. ', $normalText);
         $paragraph->addSpan('Die meisten Readers sind auch ganz gut darin; schliesslich gibt der Nutzer dem PDF-Viewer Schuld, wenn etwas nicht funktioniert, und nicht dem Generator.', $normalText);
         $paragraphs[] = $paragraph;
 
-        $paragraph = new Text();
+        $paragraph = new Text(alignment: Alignment::ALIGNMENT_CENTER);
         $paragraph->addSpan('Eine Abstraktionsebene höher gibt es dann einen Header (die PDF Version), einen Trailer mit der Cross Reference Table (Byte Offsets zu den verschiedenen Teilen des PDFs) und den Body (mit dem ganzen Inhalt). ', $normalText);
         $paragraph->addSpan('Die Cross Reference Table war früher einmal nützlich, um die relevanten Teile des PDFs schnell anzuzeigen. ', $normalText);
         $paragraph->addSpan('Bei aktuellen Readers wird diese Sektion aber vermutlich ignoriert; auch komplett falsche Werte haben keinen Einfluss auf die Darstellung. ', $normalText);
+        $paragraphs[] = $paragraph;
+
+        $paragraph = new Text(alignment: Alignment::ALIGNMENT_LEFT);
         $paragraph->addSpan('Als Inhaltsarten gibt es nenneswerterweise Bilder, Text und Schriftarten. ', $normalText);
         $paragraph->addSpan('Jeder dieser Inhalte ist an eine jeweilige "Page" gebunden, mit spezifizierten x/y Koordinaten. ', $normalText);
         $paragraph->addSpan('Ganz nach PDF-Konzept gibts hier keine magic: Alle Angaben sind absolut und keine automatische Zentrierung oder Skalierung wird angeboten.', $normalText);
         $paragraphs[] = $paragraph;
 
-        $outerFlow = new Flow(FlowDirection::ROW, 10);
+        $outerFlow = new Flow(FlowDirection::ROW, 4);
         $flow = new Flow(FlowDirection::COLUMN);
-        $flow->setWidth(85);
+        $flow->setWidth(98);
         for ($i = 0; $i < 30; ++$i) {
             foreach ($paragraphs as $paragraph) {
                 $paragraph->setMargin([0, 3 * 1.6, 0, 0]);
+                $paragraph->setStyle(new ElementStyle(backgroundColor: new Color(200, 300, 0)));
                 $flow->add($paragraph);
             }
         }
@@ -135,8 +139,8 @@ class FlowTest extends TestCase
 
         // assert
         $result = $this->render($document);
-        $this->assertStringContainsString('1 0 0 1 0 -60.96 cm BT', $result);
-        $this->assertStringContainsString('(Jeder dieser)Tj (Inhalte', $result);
-        $this->assertStringContainsString('1 0 0 1 0 -60.96 cm BT (Eine', $result);
+        $this->assertStringContainsString('0.821 Tw 5.76 TL (PDF ist ein Te', $result);
+        $this->assertStringContainsString('15.348 -5.76 TD (das Format von solchen simplen', $result);
+        $this->assertStringContainsString('5.34 -5.76 TD (ganz gut darin; schliesslich gibt', $result);
     }
 }
